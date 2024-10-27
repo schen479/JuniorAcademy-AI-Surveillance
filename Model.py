1162ae201de0f8dd74cd9d3d292eb1ef791e0e19
Stephen Chen <stephen.chen420@gmail.com>
2024-10-26 21:15:18 -0400

    template for functions

--- Model.py
+++ Model.py
@@ -1,1 +1,19 @@
 #File for AI model 
+
+#Download these libraries if you don't have them install already
+import cv2
+import torch
+import openpose
+
+objs = []
+
+#Get objects of the shelves that are subjected to shoplifting using YOLO. 
+def get_Obj(frame): 
+
+#Returns the points of a person's body using OpenPose
+def get_Pose_Points(frame):
+
+#Takes in the body points and location of items in the store and
+#detects for shoplifting if items are too close to certain body parts
+def detect_Anomaly(objects, pose_points):
+
--- Processing.py
+++ Processing.py
@@ -1,1 +1,15 @@
 #File for video processing and filtering
+
+#Blur people faces if anomaly isn't detected
+def blurFaces():
+
+#Send a video to one of two server (local servers used for testing). If anomaly is detected, send to central
+#if no anomaly is detected, save temporarily in local server. 
+def send_Video():
+
+#Deletes all footage if no anomaly is detected in 5 minutes
+def delete():
+
+#Main function for parsing a video, dividing it into smaller clips, and running the Ai model to
+#see if there's an anomaly in each clip 
+def process_Video(video_path):
