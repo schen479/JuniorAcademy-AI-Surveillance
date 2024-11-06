6bbecb868b4face343d9e7c65c75ce907680d704
Stephen Chen <stephen.chen420@gmail.com>
2024-11-06 13:06:19 -0500

    adding changes from session

--- Model.py
+++ Model.py
@@ -3,17 +3,60 @@
 #Download these libraries if you don't have them install already
 import cv2
 import torch
-import openpose
+import mediapipe as mp
 
-objs = []
+import datetime
+import pytz
 
+model_path = "JuniorAcademy-AI-Surveillance\pose_landmarker_full.task"
+
+BaseOptions = mp.tasks.BaseOptions
+PoseLandmarker = mp.tasks.vision.PoseLandmarker
+PoseLandmarkerOptions = mp.tasks.vision.PoseLandmarkerOptions
+VisionRunningMode = mp.tasks.vision.RunningMode
+
+options = PoseLandmarkerOptions(
+    base_options=BaseOptions(model_asset_path=model_path),
+    running_mode=VisionRunningMode.VIDEO)
+
+with PoseLandmarker.create_from_options(options) as landmarker:
+
+#Shubh
 #Get objects of the shelves that are subjected to shoplifting using YOLO. 
 def get_Obj(frame): 
+    # define objects ex. soda cans, box of cereals
 
+
+    
+#Stephen
 #Returns the points of a person's body using OpenPose
 def get_Pose_Points(frame):
 
+#Alan
 #Takes in the body points and location of items in the store and
 #detects for shoplifting if items are too close to certain body parts
 def detect_Anomaly(objects, pose_points):
+    # check if object is in hand of person
+    
+    # 
+
+# Kevin
+#Alert owner of shoplifting event to their messenges
+'''
+Parameters:
+    reason - string - possible reasons of found in ['stealing', 'violating products']
+    suspectDesc - dictionary describing suspect's details
+        'race', 'hairColor', 'isWearingGlasses', 'shirtColor', 'leggingColor', 'height'
+'''
+def alertOwner(reason, suspectDesc):
+
+    # send text
+    current_time = datetime.datetime.now(tz=pytz.UTC)
+    message = "Shoplifting Detected at: " + str(current_time) + "\n Subject is possibly " + reason
+    suspectDescText = "Subject is "
+
+    print(current_time)
+    print(message)
+    pri
+    # send video frame of event
 
--- Processing.py
+++ Processing.py
@@ -1,15 +1,28 @@
 #File for video processing and filtering
 
+#Alan
 #Blur people faces if anomaly isn't detected
 def blurFaces():
+    # define key points of the face
 
+    # blur within key points
+    
+    # move the box blur along face
+
+#Stephen
 #Send a video to one of two server (local servers used for testing). If anomaly is detected, send to central
 #if no anomaly is detected, save temporarily in local server. 
 def send_Video():
 
-#Deletes all footage if no anomaly is detected in 5 minutes
-def delete():
 
+#Shubh
+#Deletes all footage if no anomaly is detected in 5 minutes
+def delete_Footage():
+    
+
+#Stephen
 #Main function for parsing a video, dividing it into smaller clips, and running the Ai model to
 #see if there's an anomaly in each clip 
 def process_Video(video_path):
+
+
